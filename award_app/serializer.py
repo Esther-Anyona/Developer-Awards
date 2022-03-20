c3dc3a91e2eb8b8da3b79934a16f12c622e2c3fc
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-20 06:02:07 +0300

    Add serializer to return profile and project fields

--- award_app/serializer.py
+++ award_app/serializer.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+from .models import Profile, Project
+from dataclasses import fields
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'profile_pic', 'bio', 'contact')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('author', 'project_image', 'title', 'description', 'profile', 'project_url', 'date_created', 'location', 'tags', 'like')
