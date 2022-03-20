812059fc93b4bfd75ca605ca06341842c06aab66
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-20 04:36:16 +0300

    Add two-step authentication

--- award_project/urls.py
+++ award_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('award_app.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
